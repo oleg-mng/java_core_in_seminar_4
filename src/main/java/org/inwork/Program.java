f3a8b9fbe64985a19d8bde991f00069e7db9a517
oleg <legik7127@gmail.com>
2023-06-17 23:40:28 +0600

    added fileReader.read(buf)

--- src/main/java/org/inwork/Program.java
+++ src/main/java/org/inwork/Program.java
@@ -3,18 +3,20 @@ package org.inwork;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Program {
     public static void main(String[] args) {
-        try {
-            int[] nums = new int[3];
-            nums[3] = 57;
-            System.out.println(nums[3]);
-        }
-        catch (ArrayIndexOutOfBoundsException ex){
-            ex.toString();
-//            ex.printStackTrace();
-        }
+        readTextFile("/Users/olegmonogarov/GeekBrains_/java/java-core/6_test.txt");
+//        try {
+//            int[] nums = new int[3];
+//            nums[3] = 57;
+//            System.out.println(nums[3]);
+//        }
+//        catch (ArrayIndexOutOfBoundsException ex){
+//            ex.toString();
+////            ex.printStackTrace();
+//        }
 
     }
     public static void readTextFile(String fileName)  {
@@ -23,8 +25,16 @@ public class Program {
             char[] buf = new char[256];
             int c;
             while ((c = fileReader.read(buf)) > 0) {
+                if (c < 256){
+                    buf = Arrays.copyOf(buf, c);
+                }
+                for (char c1 : buf) {
+                    System.out.print(c1);
+
+                }
 
             }
+            fileReader.close();
         }
         catch (FileNotFoundException ex){
             System.out.println("* Сложности при открытии файла *");
